d0b80f439bd0a46aeb92cc7c9c7fa0bce52b87e1
ComputadorasySensores <79330985+ComputadorasySensores@users.noreply.github.com>
2025-01-09 10:52:34 -0300

    Add files via upload

---
+++ detector_radar.py
@@ -0,0 +1,30 @@
+from machine import Pin
+import time
+
+gp_led = 14  # Define GP de LED
+gp_sensor = 15 # Definen GP de sensor
+
+led = Pin(gp_led, Pin.OUT) # El LED se lo define como salida
+sensor = Pin(gp_sensor, Pin.IN) # El sensor se lo define como de entrada
+
+estado = 0
+val = 0
+
+while True:
+    val = sensor.value()  # lee valor del sensor
+
+    if val == 1:  
+        led.on()  
+
+        if estado == 0:
+            print("Movimiento Detectado!")
+            estado = 1  # actualiza variable a Alto
+    else:
+        led.off()  
+
+        if estado == 1:
+            print("Movimiento Detenido!")
+            estado = 0  # actualiza la variable a Bajo
+
+    time.sleep(0.2)
+    
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Detector de presencia con Raspberry Pi Pico y sensor RCWL-0516
+
+Código en MicroPython para detectar movimiento, si bien se utiliza la Raspberry Pi Pico 2W con el RP2350 puede usarse cualquiera de los modelo de Raspberry Pi Pico con el RP2040
+
+![Esquematico](esquematico.jpg)
+
+
+# Paso a paso
+
+La explicación completa la podrás ver en el siguiente video de Youtube:
+
+
